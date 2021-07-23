878833f485a6d7be7c64aa20f54488f369f45455
Samson Akanbi <samson.akanbi@outlook.com>
2021-07-23 08:35:31 +0100

    Hashed the password if a new or modified user is logging in for the first time before saving to the database.

--- src/routes.ts
+++ src/routes.ts
@@ -2,4 +2,16 @@ import { Express, Request, Response } from "express";
 
 export default function(app: Express) {
     app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200));
+
+    // Register User
+    // POST /api/user
+
+    // Login
+    // POST /api/sessions
+
+    // Get the user's sessions
+    // GET /api/sessions
+
+    // Logout
+    // DELETE /api/sessions
 }
