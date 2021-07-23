58f3370b560509c03fbc5d0807234e7249a2bcb6
Samson Akanbi <samson.akanbi@outlook.com>
2021-07-23 18:42:58 +0100

    Creation of users or user registration is now possible.

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Express, Request, Response } from "express";
 import { createUserHandler } from "./controller/user.controller";
 import validateRequest from "./middleware/validateRequest";
+import { createUserSchema } from "./schema/user.schema";
 
 export default function(app: Express) {
     app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200));
