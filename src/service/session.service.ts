de01a838ca6e39403008ea1a50b74ee6f5817b42
Samson Akanbi <samson.akanbi@outlook.com>
2021-07-23 22:34:10 +0100

    Created and exported the sign function.

---
+++ src/service/session.service.ts
@@ -0,0 +1,31 @@
+import { UserDocument } from '../model/user.model';
+import config from 'config';
+import { Omit } from "lodash";
+import Session, { SessionDocument } from "../model/session.model";
+import { LeanDocument } from 'mongoose';
+
+export async function createSession(userId: string, userAgent: string) {
+    const session = await Session.create({ user: userId, userAgent });
+
+    return session.toJSON();
+}
+
+export function createAccessToken({
+    user,
+    session,
+}: {
+    user:
+        | Omit<UserDocument, "password">
+        | LeanDocument<Omit<UserDocument, "password">>;
+    session:
+        | Omit<SessionDocument, "password">
+        | LeanDocument<Omit<SessionDocument, "password">>;
+}) {
+    // Build and return the new access token
+    const accessToken = sign(
+        { ...user, session: session._id },
+        { expiresIn: config.get("accessTokenTtl") } // 15 minutes
+    );
+
+    return accessToken;
+}
