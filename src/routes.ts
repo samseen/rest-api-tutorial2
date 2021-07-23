01ad5258f659d4c449c0d3a2e6910dbf54d19a13
Samson Akanbi <samson.akanbi@outlook.com>
2021-07-23 18:30:33 +0100

    Defined the validateRequest middleware and imported it in the route.

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Express, Request, Response } from "express";
 import { createUserHandler } from "./controller/user.controller";
+import validateRequest from "./middleware/validateRequest";
 
 export default function(app: Express) {
     app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200));
