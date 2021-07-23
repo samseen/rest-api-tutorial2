1ed5b342ae315ae7a1f10d0756c5453e3a12a0d1
Samson Akanbi <samson.akanbi@outlook.com>
2021-07-23 22:12:29 +0100

    Created the validatePassword function in user's service.

---
+++ src/controller/session.controller.ts
@@ -0,0 +1,11 @@
+export async function createUserSessionHandler(req: Request, res: Response) {
+    // Validate the email and password
+
+    // Create a session
+
+    // Create access token
+
+    // Create refresh token
+
+    // Send refresh and access token back
+}
