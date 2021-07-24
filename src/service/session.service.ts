67246be488abab4ff82455e6073bd9a9c5fc21f4
Samson Akanbi <samson.akanbi@outlook.com>
2021-07-24 11:45:34 +0100

    Created the findUser function in the user service file.

--- src/service/session.service.ts
+++ src/service/session.service.ts
@@ -1,9 +1,9 @@
 import { UserDocument } from '../model/user.model';
 import config from 'config';
-// import { Omit } from "lodash";
+import { get } from "lodash";
 import Session, { SessionDocument } from "../model/session.model";
 import { LeanDocument } from 'mongoose';
-import { sign } from '../utils/jwt.utils';
+import { decode, sign } from '../utils/jwt.utils';
 
 export async function createSession(userId: string, userAgent: string) {
     const session = await Session.create({ user: userId, userAgent });
@@ -27,6 +27,31 @@ export function createAccessToken({
         { ...user, session: session._id },
         { expiresIn: config.get("accessTokenTtl") } // 15 minutes
     );
+
+    return accessToken;
+}
+
+export async function reIssueAccessToken({
+    refreshToken,
+}: {
+    refreshToken: string;
+}) {
+    // Decode the refresh token
+    const { decoded } = decode(refreshToken);
+
+    if (!decoded || !get(decoded, "_id")) return false;
+
+    // Get the session
+    const session = await Session.findById(get(decoded, "_id"));
+
+    // Make sure the session is still valid
+    if (!session || !session?.valid) return false;
+
+    const user = await findUser({ _id: session.user });
+
+    if (!user) return false;
+
+    const accessToken = createAccessToken({ user, session });
 
     return accessToken;
 }
