358405a4c3179034625e6e3a4882dcf7aff436c8
Samson Akanbi <samson.akanbi@outlook.com>
2021-07-24 11:32:32 +0100

    Created a decode function in the jwt file to verify if the token was originally issued.

--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -6,3 +6,18 @@ const privateKey = config.get('privateKey') as string;
 export function sign(object: Object, options?: jwt.SignOptions | undefined) {
     return jwt.sign(object, privateKey, options);
 }
+
+export function decode(token: string) {
+    try {
+        const decoded = jwt.verify(token, privateKey);
+
+        return { valid: true, expired: false, decoded };
+    } catch (error) {
+        console.log({ error });
+        return {
+            valid: false,
+            expired: error.message === "jwt expired",
+            decoded: null,
+        };
+    }
+}
