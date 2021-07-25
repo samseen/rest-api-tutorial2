3674c4ab160b0f6df84239c54ad0e0feba7494c6
Samson Akanbi <samson.akanbi@outlook.com>
2021-07-25 22:12:01 +0100

    Completed the Post CRUD operations.

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Express, Request, Response } from "express";
+import { createPostHandler, updatePostHandler, getPostHandler, deletePostHandler } from "./controller/post.controller";
 import { createUserHandler } from "./controller/user.controller";
 import { createUserSessionHandler, invalidateUserSessionHandler, getUserSessionHandler } from "./controller/session.controller";
 import { validateRequest, requiresUser } from "./middleware";
 import { createUserSchema, createUserSessionSchema } from "./schema/user.schema";
+import { createPostSchema, updatePostSchema, deletePostSchema } from "./schema/post.schema";
 
 export default function(app: Express) {
     app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200));
