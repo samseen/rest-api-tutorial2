8d3097fe9f252ab5aa0318827ae4b6b09365d04c
Samson Akanbi <samson.akanbi@outlook.com>
2021-07-25 21:13:16 +0100

    Built the updatePostHandler.

--- src/controller/post.controller.ts
+++ src/controller/post.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { get } from "lodash";
-import { createPost } from "./services/post.service";
+import { createPost, findPost } from "./services/post.service";
 
 export async function createPostHandler(req: Request, res: Response) {
     const userId = get(req, "user._id");
@@ -11,7 +11,20 @@ export async function createPostHandler(req: Request, res: Response) {
     return res.send(post);
 }
 
-export async function updatePostHandler(req: Request, res: Response) {}
+export async function updatePostHandler(req: Request, res: Response) {
+    const postId = get(req, "params.postId");
+    const update = req.body;
+
+    const post = await findPost({ postId });
+
+    if(!post) {
+        return res.sendStatus(404);
+    }
+
+    const updatedPost = await findAndUpdate({ postId }, update, { new: true });
+
+    return res.send(updatedPost)
+}
 
 export async function getPostHandler(req: Request, res: Response) {}
 
